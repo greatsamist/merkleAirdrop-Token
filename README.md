34145afb11a2359b4941ea13cb96e8d85a387572
Samuel Ojo <ifegbenga@gmail.com>
2022-03-19 03:00:07 +0100

    merkle proof contract

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Basic Sample Hardhat Project
 
+<b>Contract Address: 0x6e224E2d26bf702578648f7BDb2fcCFB3cf63F31<b/>
+
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
 
 Try running some of the following tasks:
--- contracts/merkleTokenProof.sol
+++ contracts/merkleTokenProof.sol
@@ -1,33 +1,34 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0 <0.9.0;
 
-// import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MerkleVerify is ERC20 {
+    bytes32 public root =
+        0x03d62931259cfa8705be665a34eae4655560be32efcebf81891043a343c525f9;
+
+    mapping(address => bool) TokenClaimed;
+
     constructor() ERC20("merkleToken", "MKT") {}
 
-    function mint() public {
-        _mint(msg.sender, 100 * 10**18);
+    function checkValid(
+        bytes32[] calldata _merkleProof,
+        uint256 _itemId,
+        uint256 _amount
+    ) public {
+        require(!TokenClaimed[msg.sender], "Address has already claimed");
+        bytes32 leaf = keccak256(
+            abi.encodePacked(msg.sender, _itemId, _amount)
+        );
+        require(
+            MerkleProof.verify(_merkleProof, root, leaf),
+            "Incorrect proof"
+        );
+        // Mint token
+        _mint(msg.sender, _amount * 10**18);
+
+        // Mark address has claimed
+        TokenClaimed[msg.sender] = true;
     }
-
-    // function checkValid(
-    //     bytes32[] calldata _merkleProof,
-    //     uint256 _itemId,
-    //     uint256 _amount 
-    // ) public {
-    //     require(!TokenClaimed[msg.sender], "Address has already claimed");
-    //     bytes32 leaf = keccak256(
-    //         abi.encodePacked(msg.sender, _itemId, _amount)
-    //     );
-    //     require(
-    //         MerkleProof.verify(_merkleProof, root, leaf),
-    //         "Incorrect proof"
-    //     );
-    //     // Mint token
-    //     _mint(msg.sender, 100 * 10**18);
-
-    //     // Mark address has claimed
-    //     TokenClaimed[msg.sender] = true;
-    // }
 }
